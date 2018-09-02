aa3d41407ffa644ffdb3b52ae722321cd46bafe9
peter <peter@luciddg.com>
2018-09-02 01:33:48 +0100

    Add initial rules for React

--- index.js
+++ index.js
@@ -20,30 +20,32 @@ module.exports = {
 
   // http://eslint.org/docs/user-guide/configuring#configuring-plugins
   plugins: [
-    // 'compat',
-    // 'deprecate',
-    // 'import',
-    // 'jsx-a11y',
-    // 'react',
-    // 'xss',
-    // jest
-    // unicorn?
-    // array-func
-    // immutable? fp? pretty restrictive
-    // filenames
-    // disable rules that conflict with prettier
+    /*
+     * 'compat',
+     * 'deprecate',
+     * 'import',
+     * 'jsx-a11y',
+     * 'xss',
+     * jest
+     * unicorn?
+     * array-func
+     * immutable? fp? pretty restrictive
+     * filenames
+     */
   ],
 
   // http://eslint.org/docs/user-guide/configuring#extending-configuration-files
   extends: [
     require.resolve('./rules/eslint/index.js'),
-    // 'prettier/react',
-    // 'plugin:compat/recommended',
-    // 'plugin:eslint-comments/recommended',
-    // 'plugin:import/errors',
-    // 'plugin:jsx-a11y/recommended',
-    // 'plugin:react/recommended',
+    require.resolve('./rules/react/index.js'),
+    /*
+     * 'plugin:compat/recommended',
+     * 'plugin:eslint-comments/recommended',
+     * 'plugin:import/errors',
+     * 'plugin:jsx-a11y/recommended',
+     */
 
     'prettier',
+    'prettier/react',
   ],
 };
